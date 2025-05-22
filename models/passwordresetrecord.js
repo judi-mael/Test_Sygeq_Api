ea95896084a0c21673ca8f46b4842156c6d65620
jsohou <maelsohou@gmail.com>
2025-05-22 12:56:02 +0100

    Ajout des champs 'createdBy', 'updatedBy', 'deletedBy', 'restoredBy' et 'suspensionComment' dans le modèle PasswordResetRecord, et amélioration de l'indentation du code.

--- models/passwordresetrecord.js
+++ models/passwordresetrecord.js
@@ -1,20 +1,20 @@
 'use strict';
 const {
-  Model
+    Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class PasswordResetRecord extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
+    class PasswordResetRecord extends Model {
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
+        static associate(models) {
+            // define association here
+        }
     }
-  }
-  PasswordResetRecord.init({
-    id: {
+    PasswordResetRecord.init({
+        id: {
             type: DataTypes.BIGINT(20),
             primaryKey: true,
             autoIncrement: true
@@ -61,12 +61,32 @@ module.exports = (sequelize, DataTypes) => {
             type: DataTypes.DATE,
             allowNull: true
         },
-  }, {
-    sequelize,
-    modelName: 'PasswordResetRecord',
-    paranoid: true,
-        timestamps:true,
-        hooks:{
+        createdBy: {
+            type: DataTypes.INTEGER(11),
+            allowNull: false
+        },
+        updatedBy: {
+            type: DataTypes.INTEGER(11),
+            allowNull: false
+        },
+        deletedBy: {
+            type: DataTypes.INTEGER(11),
+            allowNull: true
+        },
+        restoredBy: {
+            type: DataTypes.INTEGER(11),
+            allowNull: true
+        },
+        suspensionComment: {
+            type: DataTypes.TEXT,
+            allowNull: true
+        },
+    }, {
+        sequelize,
+        modelName: 'PasswordResetRecord',
+        paranoid: true,
+        timestamps: true,
+        hooks: {
             beforeCreate: async (instance, options) => {
                 instance.createdAt = new Date();
                 instance.updatedAt = new Date();
@@ -75,6 +95,6 @@ module.exports = (sequelize, DataTypes) => {
                 instance.updatedAt = new Date();
             }
         }
-  });
-  return PasswordResetRecord;
+    });
+    return PasswordResetRecord;
 };
